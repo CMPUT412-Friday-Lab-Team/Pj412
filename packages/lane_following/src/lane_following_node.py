ff3598dbf2e5cd8ba47126ebaf439e5f81dec51b
Leen-Alzebdeh <alzebdeh@ualberta.ca>
2023-04-12 13:29:54 -0600

    implement callback and wait for duckie cross

--- packages/lane_following/src/lane_following_node.py
+++ packages/lane_following/src/lane_following_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import json
 import rospy
 
 from duckietown.dtros import DTROS, NodeType
@@ -63,6 +64,7 @@ class LaneFollowNode(DTROS):
                                    queue_size=1)
         self.sub = rospy.Subscriber(f"/{self.veh}/camera_node/image/compressed", CompressedImage,
                                     self.callback, queue_size=1, buff_size="20MB")
+        self.obj_sub = rospy.Subscriber(f'/{HOST_NAME}/detectron2_duckiebot/detected_objects', String, self.object_callback)
         self.tag_distance_sub = rospy.Subscriber(f'/{HOST_NAME}/detected_tag_distance', String, self.tag_distance_callback)
         self.general_sub = rospy.Subscriber('/general', String, self.general_callback)
         self.general_pub = rospy.Publisher('/general', String, queue_size=1)
@@ -78,6 +80,15 @@ class LaneFollowNode(DTROS):
         if tagid == 163 and distance < .4:
             self.lock.acquire()
             self.crosswalk_tag_detected = True
+            self.lock.release()
+
+    def object_callback(self, msg):
+        msg_json = json.loads(msg.data) or False
+        if msg_json:
+            self.lock.acquire() ## do i keep this?
+            self.obj_class = msg_json["class"]
+            self.obj_scores = msg_json["scores"]     
+            self.obj_boxes = msg_json["pred_boxes"]       
             self.lock.release()
 
     def general_callback(self, msg):
@@ -183,6 +194,9 @@ class LaneFollowNode(DTROS):
                     self.general_pub.publish(String('shutdown'))  # TODO: start part 3 parking node
             else:
                 self.controller.stop(20)
+                timer = 20
+                while 0 in self.obj_class or timer>0:
+                    timer -= 1
                 new_stateid = self.bot_state.advance_state()
                 pass  # TODO: wait for ducks to cross if any
 
